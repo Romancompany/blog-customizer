f0e3a570f2d92cd47d839d1d63593ebb2cf5cb91
Roman <romancompany@mail.ru>
2024-10-13 20:04:29 +0300

    сделал кнопку применить и сбросить

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,8 +2,7 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
-//import { StoryDecorator } from '../story-decorator';
+import { useState, SyntheticEvent } from 'react';
 import { Separator } from '../separator';
 import { Text } from '../text';
 import { Select } from '../select';
@@ -14,30 +13,66 @@ import {
 	backgroundColors,
 	contentWidthArr,
 	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
 } from '../../constants/articleProps';
 
-const startOpen = true;
+type ArticleParamsFormProps = {
+	setArticleState: (articleState: ArticleStateType) => void;
+};
 
-export const ArticleParamsForm = () => {
-	const [isOpen, setOpen] = useState(startOpen);
+export const ArticleParamsForm = ({
+	setArticleState,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setOpen] = useState(false);
 	const arrowClick = () => {
 		setOpen((preOpen) => !preOpen);
 	};
+	const [fontFamily, setFontFamily] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(
+		defaultArticleState.contentWidth
+	);
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
 
-	const [fontFamily, setFontFamily] = useState(fontFamilyOptions[0]);
-	const [fontColor, setFontColor] = useState(fontColors[0]);
-	const [backgroundColor, setBackgroundColor] = useState(backgroundColors[0]);
-	const [contentWidth, setContentWidth] = useState(contentWidthArr[0]);
-	const [fontSize, setFontSize] = useState(fontSizeOptions[0]);
+	const resetClick = () => {
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+		setFontSize(defaultArticleState.fontSizeOption);
+	};
+
+	const commitClick = () => {
+		const articleState: ArticleStateType = {
+			fontFamilyOption: fontFamily,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+			fontSizeOption: fontSize,
+		};
+
+		setArticleState(articleState);
+	};
 
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={arrowClick} />
 			<aside
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
-				<form className={styles.form}>
+				<form
+					className={styles.form}
+					onSubmit={(e: SyntheticEvent) => {
+						e.preventDefault();
+					}}>
 					<Text
 						as={'h2'}
+						size={31}
 						weight={800}
 						uppercase={true}
 						align={'left'}
@@ -76,10 +111,9 @@ export const ArticleParamsForm = () => {
 						options={contentWidthArr}
 						title='ШИРИНА КОНТЕНТА'
 					/>
-
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' type='reset' onClick={resetClick} />
+						<Button title='Применить' type='submit' onClick={commitClick} />
 					</div>
 				</form>
 			</aside>
